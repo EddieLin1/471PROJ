afd9362c3ed491b24ecb8a611e323dc1011da204
Edrick Lin <edrick.lin28@gmail.com>
2025-04-18 18:41:38 -0600

    formatting/viewing update

--- app.py
+++ app.py
@@ -7,17 +7,17 @@ app = Flask(__name__)
 def home():
     if request.method == "POST":
         name = request.form["name"]
-        with sqlite3.connect("database.db") as conn:
-            conn.execute("INSERT INTO users (name) VALUES (?)", (name,))
+        with sqlite3.connect("Homeapp.db") as conn:
+            conn.execute("INSERT INTO person (name) VALUES (?)", (name,))
     
     # Fetch users
-    with sqlite3.connect("database.db") as conn:
-        users = conn.execute("SELECT * FROM users").fetchall()
+    with sqlite3.connect("Homeapp.db") as conn:
+        users = conn.execute("SELECT * FROM person").fetchall()
     
     return render_template("index.html", users=users)
 
 @app.route("/login", methods=["GET", "POST"])
-def desired_route():
+def login():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
@@ -29,6 +29,38 @@ def desired_route():
         return render_template("Login.html", error="Invalid username or password.")
     
     return render_template('Login.html')
+
+@app.route("/contractor-view", methods=["GET"])
+def contractor():
+    # Fetch employees
+    with sqlite3.connect("Homeapp.db") as conn:
+        emps = conn.execute("SELECT * FROM employee").fetchall()
+    
+    return render_template("ContractorView.html", emps=emps)
+
+@app.route("/lease-agreement-view", methods=["GET"])
+def leaseAgreement():
+    # Fetch employees
+    with sqlite3.connect("Homeapp.db") as conn:
+        las = conn.execute("SELECT * FROM leaseagreement").fetchall()
+    
+    return render_template("LeaseAgreementView.html", las=las)
+
+@app.route("/property-view", methods=["GET"])
+def property():
+    # Fetch employees
+    with sqlite3.connect("Homeapp.db") as conn:
+        ps = conn.execute("SELECT * FROM property").fetchall()
+    
+    return render_template("PropertyView.html", ps=ps)
+
+@app.route("/room-view", methods=["GET"])
+def room():
+    # Fetch employees
+    with sqlite3.connect("Homeapp.db") as conn:
+        rs = conn.execute("SELECT * FROM room").fetchall()
+    
+    return render_template("RoomView.html", rs=rs)
 
 
 if __name__ == '__main__':
